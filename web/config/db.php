ab6a7586b83c936bb8538ffb57be5dfcccc917d8
arcoverde <alvaro.vieira@gmail.com>
2017-03-23 11:20:11 -0300

    Dados do BD em arquivo externo

--- web/config/db.php
+++ web/config/db.php
@@ -1,9 +1,17 @@
 <?php
 
+if (!YII_ENV_DEV) {
+    $dns = 'mysql:host=127.0.0.1;dbname=gibiteca';
+    $username = 'root';
+    $password = '';
+} else {
+    list($dns, $username, $password) = explode('|', file_get_contents(__DIR__ . '/db.config'));
+}
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=gibiteca',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => $dns,
+    'username' => $username, 
+    'password' => $password,
     'charset' => 'utf8',
 ];
